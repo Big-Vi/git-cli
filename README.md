f8019d4835c2c96d9e2dc2ab3f2e563a13eb80ca
big-vi <vignesh.murugan@hotmail.com>
2023-02-17 13:33:15 +1300

    code tidy up

--- README.md
+++ README.md
@@ -19,7 +19,15 @@ Open ``htmlcov/index.html`` in your browser to explore the report.
 
 `pip-compile-multi` -> to compile multiple requirements files to lock dependency version.
 
-TODO:
+## How login with GitHub works
+
+-   When you run `git-cli login`, an application sends an request to `https://github.com/login/device/code` to get the user verification code and sends the user to authorization URL(`https://github.com/login/device`) where the user will enter the verification code.
+-   The app continuously polls this URL(`https://github.com/login/oauth/access_token`) to get an access token once the user authorized the device. An access token gets stored in `.git-cli` config file on your computer.
+-   The app uses this access token to query(GraphQL) your GitHub repository.
+
+
+## TODO:
+
 -   Popular repositories(based on Stars/Fork/Clone)  
 -   Add testing.  
 -   Add Personal Access Token authentication.  
--- git_cli/cmd/login.py
+++ git_cli/cmd/login.py
@@ -5,9 +5,12 @@ import time
 import configparser
 import typing as t
 import webbrowser
+import sys
+import logging
 import git_cli.constants as constants
 
 config = configparser.ConfigParser()
+logger = logging.getLogger(__name__)
 
 @click.command()
 def login() -> None:
@@ -16,31 +19,35 @@ def login() -> None:
         print("GitHub is already authenticated")
         return None
 
-    # Define the parameters for the authorization URL
-    verification_params = {
-        "client_id": constants.CLIENT_ID,
-        'scope': "read:org repo gist"
-    }
-    headers = {'Accept': 'application/json'}
-
-    response = requests.post(
-        constants.VERIFICATION_URL, headers=headers, data=verification_params)
-
     try:
+        # Define the parameters for the authorization URL
+        verification_params = {
+            "client_id": constants.CLIENT_ID,
+            'scope': "read:org repo gist"
+        }
+        request_headers: t.Dict[str, str] = {'Accept': 'application/json'}
+
+        response = requests.post(
+            constants.VERIFICATION_URL, headers=request_headers, data=verification_params)
+
         print("Enter this code in the browser " + response.json()["user_code"])
         # Open the authorization URL in the default web browser
         click.pause()
         webbrowser.open(response.json()["verification_uri"])
+
     except Exception as e:
-        print(e)
+        logger.error(e)
+        sys.exit()
 
     grant_type = "urn:ietf:params:oauth:grant-type:device_code"
 
+    # Store the GitHub access token in configfile
     def store_config(data: t.Dict[str, t.Any]) -> None:
         config["DEFAULT"] = {"access_token": data["access_token"]}
         with open(constants.CONFIG_FILE_PATH, "w") as configfile:
             config.write(configfile)
 
+    # Polling access token url to get the token
     def poll() -> None:
         start_time = time.time()
         access_token_params = {"client_id": "41d4f6d801d4c40c9967", "device_code": response.json()[
